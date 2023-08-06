0c382c7e2350a200435520cbf88784ee7cdc74fc
Jan Häusler <jan.haeusler@metropolitan-cities.com>
2023-08-06 15:09:28 +0200

    feat: first iteration

--- main.rs
+++ main.rs
@@ -1,22 +1,33 @@
-use tungstenite::{connect, Message};
-use std::collections::HashMap;
+use tungstenite::connect;
 use serde_json::{Value, Error};
-use std::process::Command;
+use std::collections::HashMap;
+use wol::send_wol;
+use url::Url;
+use std::env;
 
 fn main() {
     // Define the WebSocket server URL
-    let url = "ws://your-websocket-server.com";
+    let args: Vec<String> = env::args().collect();
+
+    // Check if at least one argument is provided
+    if args.len() < 2 {
+        eprintln!("Usage: {} <websocket_server_url>", args[0]);
+        std::process::exit(1);
+    }
+
+    let url = &args[1];
 
     // Establish a connection
-    let (mut socket, _) = connect(url.parse().unwrap()).expect("Failed to connect");
+    let parsed_url = Url::parse(url).unwrap();
+    let (mut socket, _) = connect(parsed_url).expect("Failed to connect");
 
     // Define a mapping of commands to executable paths
     let mut commands_map = HashMap::new();
-    commands_map.insert("run ableton", "ableton.exe");
+    commands_map.insert("run ableton", "C:/ProgramData/Ableton/Live 11 Suite/Program/Ableton Live 11 Suite.exe");
 
     // Listen for messages
     loop {
-        let msg = socket.read_message().expect("Error reading message");
+        let msg = socket.read().expect("Error reading message");
 
         if msg.is_text() || msg.is_binary() {
             if let Err(e) = handle_command(msg.to_text().unwrap(), &commands_map) {
@@ -29,15 +40,40 @@ fn main() {
 fn handle_command(command_str: &str, commands_map: &HashMap<&str, &str>) -> Result<(), Error> {
     let v: Value = serde_json::from_str(command_str)?;
     if let Some(command_type) = v["type"].as_str() {
-        if command_type == "exec" {
-            if let Some(action) = v["action"].as_str() {
-                if let Some(exec_path) = commands_map.get(action) {
-                    Command::new(exec_path).spawn().expect("Failed to execute command");
-                } else {
-                    eprintln!("Unknown command: {}", action);
+        match command_type {
+            "exec" => {
+                if let Some(action) = v["action"].as_str() {
+                    if let Some(exec_path) = commands_map.get(action) {
+                        std::process::Command::new(exec_path).spawn().expect("Failed to execute command");
+                    } else {
+                        eprintln!("Unknown command: {}", action);
+                    }
                 }
-            }
+            },
+            "wol" => {
+                if let Some(mac_address) = v["mac"].as_str() {
+                    // Assuming your MAC address string is in the format "AA:BB:CC:DD:EE:FF"
+                    let bytes: Vec<u8> = mac_address
+                    .split(':')
+                    .map(|s| u8::from_str_radix(s, 16).expect("Failed to parse byte"))
+                    .collect();
+
+                    if bytes.len() != 6 {
+                        panic!("Invalid MAC address");
+                    }
+
+                    let wol_mac = wol::MacAddr([bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5]]);
+
+                    send_wol(wol_mac, None, None).unwrap_or_else(|err| {
+                        eprintln!("Failed to send Wake-on-LAN: {}", err);
+                    });
+                } else {
+                    eprintln!("MAC address not provided for Wake-on-LAN");
+                }
+            },
+            _ => eprintln!("Unknown command type: {}", command_type),
         }
     }
     Ok(())
 }
+
